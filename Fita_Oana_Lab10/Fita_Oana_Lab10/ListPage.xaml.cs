828068e2b7db75c0871102b50b31b4f3b4cf4144
OANA FITA <oana.fita@stud.ubbcluj.ro>
2020-12-14 19:50:21 +0200

    Lab11

--- Fita_Oana_Lab10/Fita_Oana_Lab10/ListPage.xaml.cs
+++ Fita_Oana_Lab10/Fita_Oana_Lab10/ListPage.xaml.cs
@@ -31,5 +31,21 @@ namespace Fita_Oana_Lab10
             await Navigation.PopAsync();
         }
 
+        async void OnChooseButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProductPage((ShopList)
+           this.BindingContext)
+            {
+                BindingContext = new Product()
+            });
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            var shopl = (ShopList)BindingContext;
+
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+        }
     }
 }
---
+++ Fita_Oana_Lab10/Fita_Oana_Lab10/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace Fita_Oana_Lab10.Models
+{
+    public class Product
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        public string Description { get; set; }
+
+        [OneToMany]
+        public List<ListProduct> ListProducts { get; set; }
+    }
+}
